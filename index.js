0c9900fbd5dd8a19b4ab3ffa45acb44d25d5852a
Moe-Zart <mohamedzreika03@outlook.com>
2022-07-03 20:58:20 +1000

    accessing api

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 //API: https://www.omdbapi.com/?apikey=3d6ecfb9&s=%22here%22
+//films-list inner html
+/*
+<div class="film-wrapper">
+              <figure class="film-poster-wrapper">
+                <img
+                  class="film-poster"
+                  src=""
+                  alt=""
+                />
+              </figure>
+              <div class="film-info">
+                <h3 class="film-title"></h3>
+                <div class="film-details">
+                  <p class="film-year"></p>
+                  <p class="film-type"></p>
+                </div>
+              </div>
+            </div>
+            */
+
+async function renderFilms() {
+  const films = await fetch(
+    "https:www.omdbapi.com/?apikey=3d6ecfb9&s=%22here%22"
+  );
+  const filmsData = await films.json();
+  const filmsDataHTML = filmsData.Search.map(
+      (film) =>
+        `<div class="film-wrapper">
+    <figure class="film-poster-wrapper">
+      <img
+        class="film-poster"
+        src="${film.Poster}"
+        alt=""
+      />
+    </figure>
+    <div class="film-info">
+      <h3 class="film-title">${film.Title}</h3>
+      <div class="film-details">
+        <p class="film-year">${film.Year}</p>
+        <p class="film-type">${film.Type}</p>
+      </div>
+    </div>
+  </div>`
+    )
+    .join("");
+  const filmList = document.querySelector(".films-list");
+  filmList.innerHTML = filmsDataHTML;
+  console.log(filmsData)
+}
+renderFilms();
